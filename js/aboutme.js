8cca33cb593b19e256144c7835278d2228427097
“loso762“ <loso762@naver.com>
2023-04-26 16:55:57 +0900

    헤더메뉴에 활성화 된 페이지   표시

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,24 +1,23 @@
-const arrow = document.querySelector('.arrow');
-const GoTop = document.querySelector('.GoTop');
-const timlineList = document.querySelectorAll('.timlineCon>ul>li');
+const arrow = document.querySelector(".arrow");
+const GoTop = document.querySelector(".GoTop");
+const timlineList = document.querySelectorAll(".timlineCon>ul>li");
 
-window.addEventListener("scroll",function(){
-    scrollY < 400 ? GoTop.style="opacity:0" : GoTop.style="opacity:1";
-    scrollY > 2100 ? arrow.style="opacity:0" : arrow.style="opacity:1";
-    timlineList.forEach((list,key)=>{
-        if(scrollY > 2400+(key*80)){
-            list.style="opacity:1"
-        }else{
-            list.style="opacity:0"
+window.addEventListener("scroll", function () {
+  scrollY < 400 ? (GoTop.style = "opacity:0") : (GoTop.style = "opacity:1");
+  scrollY > 2100 ? (arrow.style = "opacity:0") : (arrow.style = "opacity:1");
+  timlineList.forEach((list, key) => {
+    if (scrollY > 2400 + key * 80) {
+      list.style = "opacity:1";
+    } else {
+      list.style = "opacity:0";
+    }
+  });
+});
 
-        }
-    })
-})
-
-GoTop.addEventListener("click",function(){
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-})
+GoTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
--- js/common.js
+++ js/common.js
@@ -1,13 +1,20 @@
-$('body').prepend('<header>');
-$('header').load("./header.html header .home,nav",head);
+$("body").prepend("<header>");
+$("header").load("./header.html header .home,nav", head);
 
-
-function head(){
+function head() {
   const headerMenu = document.querySelectorAll("header > nav > a");
 
   headerMenu.forEach((menu) => {
-    menu.addEventListener("click", (e) => { 
-      menu.classList.add("active")
-    })
-  })
+    menu.addEventListener("click", (e) => {
+      menu.classList.add("active");
+    });
+  });
+
+  if (window.location.pathname.includes("aboutme.html")) {
+    const meLink = document.querySelector(".me");
+    meLink.classList.add("active");
+  } else if (window.location.pathname.includes("Projects.html")) {
+    const meLink = document.querySelector(".pro");
+    meLink.classList.add("active");
+  }
 }
