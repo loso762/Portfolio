7e59f770cf17d5a59a0a290f8718a75cd6185e5f
“loso762“ <loso762@naver.com>
2023-04-26 16:20:42 +0900

    타블렛 반응형 적용

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
-const popcorn = document.querySelector('.popcorn');
-const bg2 = document.querySelector('.bg2');
-const bg1 = document.querySelector('.bg1');
-const poster = document.querySelector('.poster');
-const poster2 = document.querySelector('.poster2');
-const posterex = document.querySelector('.posterex');
-const poster2ex = document.querySelector('.poster2ex');
-const plzclick = document.querySelector('.plzclick');
-const plzclick2 = document.querySelector('.plzclick2');
+const popcorn = document.querySelector(".popcorn");
+const bg2 = document.querySelector(".bg2");
+const bg1 = document.querySelector(".bg1");
+const p1 = document.querySelector(".p1");
+const p2 = document.querySelector(".p2");
+const posterex = document.querySelector(".posterex");
+const poster2ex = document.querySelector(".poster2ex");
+const plzclick = document.querySelector(".plzclick");
+const plzclick2 = document.querySelector(".plzclick2");
 
-poster2.addEventListener("mouseover",()=>{
+p2.addEventListener("mouseover", () => {
   popcorn.style = "transform: scale(0.5); opacity:0";
-  bg1.style = "width:100%; z-index:3"
-  poster2ex.style = "opacity: 1;transition: 0.5s;transition-delay: 0.6s; transform:scale(1);"
-  plzclick2.style = "opacity: 1;animation: blink 1s infinite alternate;transition: 0.5s;transition-delay: 0.5s;"  
-})
-poster2.addEventListener("mouseleave",()=>{
-  popcorn.style = "transform: translateX(-50%); opacity:1"
-  bg1.style = "width:50% ; z-index:2"
-  poster2ex.style = "opacity: 0; transform:scale(0);"
-  plzclick2.style = "opacity:0;"
-})
+  bg1.style = "width:100%; z-index:3";
+  poster2ex.style = "opacity: 1;transition: 0.5s;transition-delay: 0.6s; transform:scale(1);";
+  plzclick2.style =
+    "opacity: 1;animation: blink 1s infinite alternate;transition: 0.5s;transition-delay: 0.5s;";
+});
 
-poster.addEventListener("mouseover",()=>{
+p2.addEventListener("mouseleave", () => {
+  popcorn.style = "transform: translateX(-50%); opacity:1";
+  bg1.style = "width:50% ; z-index:2";
+  poster2ex.style = "opacity: 0; transform:scale(0);";
+  plzclick2.style = "opacity:0;";
+});
+
+p1.addEventListener("mouseover", () => {
   popcorn.style = "transform: scale(0.5); opacity:0";
-  bg2.style = "width:100%; z-index:3"
-  posterex.style = "opacity: 1;transition: 0.5s;transition-delay: 0.6s; transform:scale(1);"
-  plzclick.style = "opacity: 1;animation: blink 1s infinite alternate;transition: 0.5s;transition-delay: 0.5s;"  
-})
-poster.addEventListener("mouseleave",()=>{
-  popcorn.style = "transform: translateX(-50%); opacity:1"
-  bg2.style = "width:50%; z-index:2"
-  posterex.style = "opacity: 0; transform:scale(0);"
-  plzclick.style = "opacity:0;"
-})
+  bg2.style = "width:100%; z-index:3";
+  posterex.style = "opacity: 1;transition: 0.5s;transition-delay: 0.6s; transform:scale(1);";
+  plzclick.style =
+    "opacity: 1;animation: blink 1s infinite alternate;transition: 0.5s;transition-delay: 0.5s;";
+});
+
+p1.addEventListener("mouseleave", () => {
+  popcorn.style = "transform: translateX(-50%); opacity:1";
+  bg2.style = "width:50%; z-index:2";
+  posterex.style = "opacity: 0; transform:scale(0);";
+  plzclick.style = "opacity:0;";
+});
